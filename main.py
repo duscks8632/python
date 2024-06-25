150d7832afd39f037e471514ba0776e13946afde
chan8632 <duscks8632@gmail.com>
2024-06-25 11:34:59 +0900

    제일 적은 동전 갯수 그리디

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import module
+# 동전 갯수가 가장 적은 경우의 수
+n = 1260
+count = 0
+# 항상 큰 단위가 작은 단위의 배수이므로 그리디로 최적의 해를 구할 수 있음
+# 예시로 500은 100,50,10의 배수, 100은 50,10의 배수, 50은 10의 배수
+kind_of_coin = [500, 100, 50, 10]
 
-num1 = 5
-num2 = 2
+for coin in kind_of_coin:
+    count += n // coin
+    n %= coin
 
-num3 = module.mul(num1,num2)
-if num3 > 8:
-    print('hello')
+print(count)
