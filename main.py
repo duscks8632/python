a85c885efd73b3936b9f95a367e980520846ea80
chan8632 <duscks8632@gmail.com>
2024-06-26 11:14:29 +0900

    1이 될 때까지 문제 로그시간으로 푸는 방법

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-# 동전 갯수가 가장 적은 경우의 수
-n = 1260
-count = 0
-# 항상 큰 단위가 작은 단위의 배수이므로 그리디로 최적의 해를 구할 수 있음
-# 예시로 500은 100,50,10의 배수, 100은 50,10의 배수, 50은 10의 배수
-kind_of_coin = [500, 100, 50, 10]
-
-for coin in kind_of_coin:
-    count += n // coin
-    n %= coin
-
-print(count)
+# N, K을 공백을 기준으로 구분하여 입력 받기
+n, k = map(int, input().split())
+result = 0
+while True:
+    # 우선 n를 나눌 수 있는 현재 가장 큰 k를 찾는 과정
+    target = (n // k) * k
+    # 그 다음 n를 k의 가장 큰 배수가 되도록 가공하는 과정
+    result += (n - target)
+    n = target
+    # N이 K보다 작을 때 (더 이상 나눌 수 없을 때) 반복문 탈출
+    if n < k:
+        break
+    # K로 나누기
+    # 나눌 거기 때문에 연산횟수를 늘려준다.(result는 연산횟수)
+    result += 1
+    n //= k
+# n이 1이 되기 전까지 일일이 빼야하기 때문에(나누는 과정은 끝남) n - 1 번 만큼 카운트를 시켜준다.
+result += (n - 1)
+print(result)
