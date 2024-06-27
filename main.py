d05d759bceda90d51e9d5ad1d3a04397e5cc8607
chan8632 <duscks8632@gmail.com>
2024-06-27 11:50:08 +0900

    6월 27일 [그리디] 각자리 숫자 더하기 곱하기

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-n, k = map(int, input().split())
-result = 0
+data = input()
+# 초기 누적값을 result로
+result = int(data[0])
 
-while n != 1:
-    if n % k == 0:
-        n //= k
-        result += 1
+# 왼쪽부터 차례로 하나씩 연산실행 ( 더하기 먼저 안함 )
+for i in range(1, len(data)):
+    # 각자리 숫자는 num으로 세팅
+    num = int(data[i])
+    # 둘 중 하나라도 0이거나 1 이면 더하기
+    if result <= 1 or num <= 1:
+        result += num
     else:
-        n -= 1
-        result += 1
+        result *= num
 
 print(result)
+
