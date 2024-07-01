dbaa59ed07d1d1a449376f469a482a32a6582535
yeonchan <77495572+chan8632@users.noreply.github.com>
2024-07-01 14:31:18 +0000

    [구현,완전탐색] 3이 하나라도 있는 시각

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-n = int(input())
-plans = input().split()
-x, y = 1, 1
-move_types = ['L', 'R', 'U', 'D']
+h = int(input())
+count = 0
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+# h는 시간 j는 분, k는 초로 3이 한번이라도 있으면 count + 1이다.
+for i in range(h + 1):
+  for j in range(60):
+    for k in range(60):
+      if '3' in str(i) + str(j) + str(k):
+        count += 1
 
-for plan in plans:
-    for i in range(len(move_types)):
-        if plan == move_types[i]:
-            nx = x + dx[i]
-            ny = y + dy[i]
-    if nx < 1 or ny < 1 or nx > n or ny > n:
-        continue
-    else:
-        x = nx
-        y = ny
+print(count)
 
-print
