8e238ea6b00635e0ea94da7f7b4cd6586689e9ac
chan8632 <duscks8632@gmail.com>
2024-07-03 11:26:07 +0900

    [구현]알파벳과 숫자가 섞여있는 문자열 재정렬 문제

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-n = int(input())
-plans = input().split()
-x , y = 1, 1
-move_types = ['L', 'R', 'U', 'D']
+input_data = input()
+result_list = []
+value = 0
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-
-for plan in plans:
-    for i in range(len(move_types)):
-        if plan == move_types[i]:
-            nx = x + dx[i]
-            ny = y + dy[i]
-    if nx < 1 or ny < 1 or nx > n or ny > n:
-        continue
+#알파벳과 숫자가 합쳐진 문자열을 한 번에 받은 후 하나하나 비교
+for input in input_data:
+    # 현 문자가 알파벳인가 검사
+    if input.isalpha() == True:
+        result_list += input
+    # 숫자면 value에 누적
     else:
-        x = nx
-        y = ny
+        value += int(input)
 
-print(x, y)
+# 알파벳 리스트들을 순서대로
+result_list.sort()
+# 맨 처음 input()로 문자열을 받을 때, 숫자가 없어서 value에 추가할게 없지 않다면 알파벳 리스트에 숫자를 추가할 것
+if value != 0:
+    result_list.append(str(value))
+
+# 공백없이 문자열 출력
+print(''.join(result_list))
+
+
 
 
 
