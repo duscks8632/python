2d0d178774cb08268d772e17fc6b4670cd57a9b1
chan8632 <duscks8632@gmail.com>
2024-07-10 11:33:55 +0900

    dfs 동작원리

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-input_data = input()
-result_list = []
-value = 0
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 
-#알파벳과 숫자가 합쳐진 문자열을 한 번에 받은 후 하나하나 비교
-for input in input_data:
-    # 현 문자가 알파벳인가 검사
-    if input.isalpha() == True:
-        result_list += input
-    # 숫자면 value에 누적
-    else:
-        value += int(input)
+graph= [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
 
-# 알파벳 리스트들을 순서대로
-result_list.sort()
-# 맨 처음 input()로 문자열을 받을 때, 숫자가 없어서 value에 추가할게 없지 않다면 알파벳 리스트에 숫자를 추가할 것
-if value != 0:
-    result_list.append(str(value))
+visited = [False] * 9
 
-# 공백없이 문자열 출력
-print(''.join(result_list))
-
-
-
-
-
-
+dfs(graph, 1, visited)
