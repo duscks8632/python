d5ac0b23ef561142736f658075f40b13ad8f2f1f
chan8632 <duscks8632@gmail.com>
2024-07-17 11:42:00 +0900

    bfs (넓이 우선 탐색)

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-def dfs(graph, v, visited):
-    visited[v] = True
-    print(v, end=' ')
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
+from collections import deque
+def bfs(graph, start, visited):
+	# 1번 노드를 deque에 넣음
+	queue = deque([start])
+	# 1번 노드 방문처리
+	visited[start] = True
+	# queue가 빌때까지 반복
+	while queue:
+		# 큐에 있는 맨 밑 노드 뽑기
+		v = queue.popleft()
+		print(v, end=' ')
+		for i in graph[v]:
+			# 방문 안한 인접노드만 큐에 추가하기
+			if not visited[i]:
+				queue.append(i)
+				visited[i] = True
 
-graph= [
-    [],
-    [2,3,8],
-    [1,7],
-    [1,4,5],
-    [3,5],
-    [3,4],
-    [7],
-    [2,6,8],
-    [1,7]
+graph = [
+	[],
+	[2,3,8],
+	[1,7],
+	[1,4,5],
+	[3,5],
+	[3,4],
+	[7],
+	[2,6,8],
+	[1,7]
 ]
 
 visited = [False] * 9
 
-dfs(graph, 1, visited)
+bfs(graph, 1, visited)
