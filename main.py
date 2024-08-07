e304ca98c4b911f4f4eae3257284ed08f75f7660
chan8632 <duscks8632@gmail.com>
2024-08-07 11:05:43 +0900

    퀵소트 내림차순

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-A = [0, 5, 9, 7, 4, 1, 6, 2, 4, 8]
+arr = [5,7,9,0,3,1,6,2,4,8]
+def quick_sort(arr, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left = start + 1
+    right = end
+    # while문에 ()를 하는 게 안전한듯?
+    while(left <= right):
+        # 왼쪽 인덱스가 마지막 인덱스보다 작아야 한다.
+        # 같아도 된다. 왼쪽 인덱스는 끝까지 가도 된다.
+        while(left <= end and arr[left] > arr[pivot]):
+            left += 1
+        # right가 start 보단 커야한다.
+        while(right > start and arr[right] < arr[pivot]):
+        # 얜 점점 작아져야 한다.
+            right -= 1
+        if(left > right):
+            arr[pivot], arr[right] = arr[right], arr[pivot]
+        else:
+            arr[left], arr[right] = arr[right], arr[left]
+    # 분할 후 왼쪽(right가 중간 인덱스 값임)
+    # pivot의 값이 중간으로 갔을 뿐 인덱스는 그대로이기 때문
+    quick_sort(arr,start, right-1)
+    quick_sort(arr,right + 1, end)
 
-for i in range(len(A)):
-    #i가 가장 앞 원소의 인덱스
-    min_index = i
-    # 맨 앞을 제외한 그 다음 배열에서 가장 작은 값의 인덱스를 구한 다음 위치를 i와 바꾼다.
-    for j in range(i+1, len(A)):
-        if A[j] < A[min_index]:
-            min_index = j
-        A[i], A[min_index] = A[min_index], A[i]
+quick_sort(arr,0, len(arr) - 1)
 
-print(A)
+print(arr)
+
+
+
+
