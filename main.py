9cec6e10ec77141e337f4ec3e96f04567ea6b2b0
chan8632 <duscks8632@gmail.com>
2024-08-23 11:45:46 +0900

    [정렬] insertion_sort

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-def bfs(x,y):
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if graph[nx][ny] == 1:
-            graph[nx][ny] += 1
-            bfs(nx, ny)
+def insert_sort(arr):
+    # 삽입할 놈을 선택
+    for i in range(len(arr)):
+        min_index = i
+        #중괄호 안에서 젤 작은 놈 찾기
+        for j in range(i+1, len(arr)):
+            #중괄호 안에서 젤 작은 놈이 나오면 업데이트
+            if arr[j] < arr[min_index]:
+                min_index = j
+        # 이러면 min_index는 젤 작은 값을 가진 인덱스가 된다. 그때 순서 바꾸기
+        arr[i], arr[min_index] = arr[min_index], arr[i]
 
-    print(graph[n -1][m - 1])
+    return arr
 
+arr1 = [7,5,9,0,3,1,6,2,4,8]
 
-n, m = map(int, input().split())
-
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
-
-dx = [1, 0, -1, 0]
-dy = [0, -1, 0, 1]
-
-bfs(0,0)
+print(insert_sort(arr1))
