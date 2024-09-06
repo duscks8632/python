532d4d5cfd38e0282200aaf4677ab88fa3cccb53
chan8632 <duscks8632@gmail.com>
2024-09-06 10:41:12 +0900

    삽입정렬

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-def select_sort(arr):
-    # 삽입할 놈을 선택
-    for i in range(len(arr)):
-        min_index = i
-        #중괄호 안에서 젤 작은 놈 찾기
-        for j in range(i+1, len(arr)):
-            #중괄호 안에서 젤 작은 놈이 나오면 업데이트
-            if arr[j] < arr[min_index]:
-                min_index = j
-        # 이러면 min_index는 젤 작은 값을 가진 인덱스가 된다. 그때 순서 바꾸기
-        arr[i], arr[min_index] = arr[min_index], arr[i]
+def insertion_sort(arr):
+    # 두번째 원소부터 끝까지로 설정
+    for i in range(1, len(arr)):
+        # end은 삽입할 원소이다.
+        for end in range(i,0,-1):
+            #그래서 end와 end보다 하나 작은 원소를 하나씩 비교한다.
+            if arr[end] < arr[end - 1]:
+                arr[end], arr[end - 1] = arr[end - 1], arr[end]
+            #자기보다 작은 데이터를 만나면 멈춤
+            else:
+                break
 
     return arr
 
 arr1 = [7,5,9,0,3,1,6,2,4,8]
+print(insertion_sort(arr1))
 
-print(select_sort(arr1))
+#변수이름을 잘못지어서 반대로 설정함
