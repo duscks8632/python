0827aaf2110346875510958284ba859001e65c25
chan8632 <duscks8632@gmail.com>
2024-09-07 23:34:41 +0900

    잘못된 퀵소트

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-def insertion_sort(arr):
-    # 두번째 원소부터 끝까지로 설정
-    for i in range(1, len(arr)):
-        # end은 삽입할 원소이다.
-        for end in range(i,0,-1):
-            #그래서 end와 end보다 하나 작은 원소를 하나씩 비교한다.
-            if arr[end] < arr[end - 1]:
-                arr[end], arr[end - 1] = arr[end - 1], arr[end]
-            #자기보다 작은 데이터를 만나면 멈춤
-            else:
-                break
+def quick_sort(arr, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left_idx = start + 1
+    right_idx = end
+    while left_idx < right_idx:
+        while arr[pivot] < arr[left_idx] and left_idx < end:
+            left_idx += 1
+        while arr[pivot] > arr[right_idx] and right_idx > start:
+            right_idx -= 1
 
-    return arr
+        arr[left_idx], arr[right_idx] = arr[right_idx], arr[left_idx]
 
-arr1 = [7,5,9,0,3,1,6,2,4,8]
-print(insertion_sort(arr1))
+    arr[right_idx], arr[pivot] = arr[right_idx], arr[pivot]
 
-#변수이름을 잘못지어서 반대로 설정함
+    quick_sort(arr,start, pivot)
+    quick_sort(arr,pivot+1,end)
+
+arr1 = [5,7,9,0,3,1,6,2,4,8]
+print(quick_sort(arr1, 0, len(arr1)))
+
+
+
